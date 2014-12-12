94ecbdf0d66dffbeb3ecd3e43d509e15f65be42f
kudohamu <kudohamu91086@yahoo.co.jp>
2014-12-12 14:59:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+dwgo05
+====
+
+[第５回 dwanGo](http://dwgo.connpass.com/event/10247/)に参加したときに作った並列ファイルダウンローダ
+司会曰く劣化aria2的な
---
+++ dwgo05.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"github.com/codegangsta/cli"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+)
+
+func download(url string) {
+	tmp := strings.Split(url, "/")
+	fileName := tmp[len(tmp)-1]
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal Error: %s", err.Error())
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal Error: %s", err.Error())
+		return
+	}
+
+	ioutil.WriteFile(fileName, body, 0644)
+	fmt.Println("done.")
+}
+
+func main() {
+	dwgo := cli.NewApp()
+	dwgo.Name = "dwgo05"
+	dwgo.Usage = "dwgo05 [<URI>]"
+	dwgo.Action = func(c *cli.Context) {
+		wg := new(sync.WaitGroup)
+		runtime.GOMAXPROCS(runtime.NumCPU())
+		for _, url := range os.Args[1:] {
+			wg.Add(1)
+			go func(url string) {
+				defer wg.Done()
+				download(url)
+			}(url)
+			wg.Wait()
+		}
+	}
+	dwgo.Run(os.Args)
+}
