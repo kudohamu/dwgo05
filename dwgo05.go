d4ea0688852781df20caa55bac3f38f265ea7896
kudohamu <kudohamu91086@yahoo.co.jp>
2014-12-15 13:19:37 +0900

    ダウンロード開始時に標準出力に表示するように

--- dwgo05.go
+++ dwgo05.go
@@ -14,6 +14,7 @@ import (
 func download(url string) {
 	tmp := strings.Split(url, "/")
 	fileName := tmp[len(tmp)-1]
+	fmt.Println("'" + fileName + "' download...")
 
 	response, err := http.Get(url)
 	if err != nil {
